6a7733f7c121cce09398a9b17994b58b49fc2cb1
Mohammednur <mamebb2023@gmail.com>
2026-05-07 19:56:27 +0300

    finish up habbits pages

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 
-type ButtonVariant = "primary" | "outline-solid" | "ghost" | "link" | "unique";
+type ButtonVariant = "primary" | "outline-solid" | "ghost" | "link";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
@@ -26,10 +26,9 @@ const Button: React.FC<ButtonProps> = ({
       : "px-5 py-2 rounded-xl font-medium transition-all duration-200 disabled:opacity-50 disabled:cursor-not-allowed shadow-black/20";
 
   const variantStyles = {
-    unique: "",
     primary:
       "bg-linear-to-r from-cyan-500 via-blue-500 to-indigo-500 text-white hover:opacity-90 active:opacity-80 shadow-xl  hover:shadow-2x hover:scale-105",
-    outline:
+    "outline-solid":
       "border border-gray-300 text-gray-900 hover:bg-gray-50 active:bg-gray-100 bg-white",
     ghost:
       "text-gray-600 hover:text-gray-900 hover:bg-gray-100 active:bg-gray-200",
